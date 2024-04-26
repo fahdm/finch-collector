1c6d5da3a8e0972baeeb0e8ccd3dd7b19dd9fa53
Fahad Mubiru <fahadmubiru1@gmail.com>
2024-04-26 10:38:57 -0700

    define views for the application

--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Finch, Cage
+from .forms import FeedingForm
 
-# TODO Temporrary Database - Remove This After Adding Finch Model
 
-finches = [
-    { 'species': 'finches and buntings', 'breed': 'Hawfinch', 'description': 'rusty, orange-brown', 'count': 40},
-    {'species': 'finches and buntings', 'breed': 'Greenfinch', 'description': ' olive-green, but with a yellow patch on the wings and tail', 'count': 50},
-    {'species': 'finches and buntings', 'breed': 'Chaffinch', 'description': 'colourful finch that is gingery-brown', 'count': 2},
-    {'species': 'finches and buntings', 'breed': 'goldfinch', 'description': ' blue-grey crown, brown back and pink breast ', 'count': 200},
-]
 
 # Create your views here.
 
@@ -18,9 +15,66 @@ def about(request):
     return render(request,'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request,'finches/index.html',{
-
         'finches': finches
-
     })
     
+def finch_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+
+    id_list = finch.cages.all().values_list('id')
+    cages_that_dont_have = Cage.objects.exclude(id__in=id_list)
+
+    feeding_form = FeedingForm()
+    return render(request,'finches/detail.html', {
+        'finch': finch, 'feeding_form': feeding_form,
+        'cages': cages_that_dont_have
+    })
+
+def assoc_cage(request, finch_id, cage_id):
+    Finch.objects.get(id=finch_id).cages.add(cage_id)
+    return redirect('detail', finch_id=finch_id)
+
+def unassoc_cage(request, finch_id, cage_id):
+    Finch.objects.get(id=finch_id).cages.remove(cage_id)
+    return redirect('detail', finch_id=finch_id)
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ('count','name')
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches/'
+
+def add_feeding(request,finch_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.finch_id = finch_id
+        new_feeding.save()
+    return redirect('detail', finch_id=finch_id)
+
+class CageList(ListView):
+  model = Cage
+
+class CageDetail(DetailView):
+    model = Cage
+    
+
+class CageCreate(CreateView):
+  model = Cage
+  fields = '__all__'
+
+class CageUpdate(UpdateView):
+  model = Cage
+  fields = ['name', 'color','material', 'size']
+
+class CageDelete(DeleteView):
+  model = Cage
+  success_url = '/cages'
